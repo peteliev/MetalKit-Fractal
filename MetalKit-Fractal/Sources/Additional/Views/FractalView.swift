ce5d862c1480acbc533ebee8b9e5403ef5c3fd53
Zhenya Peteliev <petelev.zhenya@gmail.com>
2019-11-25 19:39:23 +0200

    Implement basic fractal view appearance.

---
+++ MetalKit-Fractal/Sources/Additional/Views/FractalView.swift
@@ -0,0 +1,27 @@
+//
+//  FractalView.swift
+//  MetalKit-Fractal
+//
+//  Created by Zhenya Peteliev on 25.11.2019.
+//  Copyright © 2019 Yevhenii Peteliev. All rights reserved.
+//
+
+import Cocoa
+
+final class FractalView: NSView {
+    
+    // MARK: - Initializerr
+    override init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        /* empty */
+    }
+}
