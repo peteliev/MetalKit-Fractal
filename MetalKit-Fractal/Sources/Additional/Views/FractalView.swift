21728e2d753bce861b7349d9e752d6a8763b810e
Zhenya Peteliev <petelev.zhenya@gmail.com>
2019-11-25 19:59:26 +0200

    Improve fractal view appearance.

--- MetalKit-Fractal/Sources/Additional/Views/FractalView.swift
+++ MetalKit-Fractal/Sources/Additional/Views/FractalView.swift
@@ -7,21 +7,70 @@
 //
 
 import Cocoa
+import MetalKit
 
-final class FractalView: NSView {
+final class FractalView: MTKView {
     
-    // MARK: - Initializerr
-    override init(frame frameRect: CGRect) {
-        super.init(frame: frameRect)
+    // MARK: - Initializers
+    override init(frame frameRect: CGRect, device: MTLDevice?) {
+        super.init(frame: frameRect, device: device)
         commonInit()
     }
     
-    required init?(coder: NSCoder) {
+    required init(coder: NSCoder) {
         super.init(coder: coder)
+        
+        device = MTLCreateSystemDefaultDevice()
         commonInit()
     }
-
+    
     private func commonInit() {
-        /* empty */
+        framebufferOnly = false
+
+        guard let device = device else { return }
+        commandQueue = device.makeCommandQueue()
+        
+        registerShaders()
+    }
+    
+    // MARK: - NSView
+    public override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        guard
+            let drawable = currentDrawable,
+            let commandQueue = commandQueue,
+            let computePipelineState = computePipelineState,
+            let commandBuffer = commandQueue.makeCommandBuffer(),
+            let commandEncoder = commandBuffer.makeComputeCommandEncoder() else { return }
+        
+        commandEncoder.setComputePipelineState(computePipelineState)
+        commandEncoder.setTexture(drawable.texture, index: 0)
+        
+        let threadGroupCount = MTLSizeMake(8, 8, 1)
+        let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height / threadGroupCount.height, 1)
+        commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+        commandEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+    
+    // MARK: - Private Properies
+    private var commandQueue: MTLCommandQueue? = nil
+    private var computePipelineState: MTLComputePipelineState? = nil
+}
+
+// MARK: - Private Methods
+private extension FractalView {
+    
+    func registerShaders() {
+        guard
+            let device = device,
+            let library = device.makeDefaultLibrary(),
+            let kernel = library.makeFunction(name: "compute"),
+            let computePipelineState = try? device.makeComputePipelineState(function: kernel) else { return }
+                
+        self.computePipelineState = computePipelineState
     }
 }
