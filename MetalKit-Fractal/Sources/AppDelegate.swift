a73fdaedefe3b3259d5f719927375de1cb14b341
Zhenya Peteliev <petelev.zhenya@gmail.com>
2019-11-25 14:27:42 +0200

    Implement basic project structure.

---
+++ MetalKit-Fractal/Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalKit-Fractal
+//
+//  Created by Zhenya Peteliev on 25.11.2019.
+//  Copyright © 2019 Yevhenii Peteliev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalKit-Fractal/Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalKit-Fractal
+//
+//  Created by Zhenya Peteliev on 25.11.2019.
+//  Copyright © 2019 Yevhenii Peteliev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
